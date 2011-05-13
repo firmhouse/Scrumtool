dc5047bfc0513ffeaf529ea579fd07805c2f4d42
Arco Overbeek <arcooverbeek@MB-Pro-van-Arco-Overbeek.local>
2011-05-13 23:12:19 +0200

    First app commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrumtool::Application.config.secret_token = '238a9ab7f1ab8fb2989e302deca11a3c4adee79b657cb0a227f76c7f3235fc858c5b968eef611a1f07c607ed20185ab2a0bfcf4bf8719effd65ceea7cc530660'
