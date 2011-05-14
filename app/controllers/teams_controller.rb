e8dc55faeb86ed2bd573446176744bd3c72702ac
Arco Overbeek <arcooverbeek@MB-Pro-van-Arco-Overbeek.local>
2011-05-14 14:39:57 +0200

    Added a team id to the user and called create instead of build

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,7 +10,7 @@ class TeamsController < ApplicationController
     params[:user_attributes][:password]
     params[:user_attributes][:password]
     if team.save
-      user = team.users.build(:login => params[:user_attributes][:email], :email => params[:user_attributes][:email], :first_name => params[:user_attributes][:first_name], :last_name => params[:user_attributes][:last_name], :password => params[:user_attributes][:password], :password_confirmation => params[:user_attributes][:password_confirmation])
+      user = team.users.create!(:team_id => team.id, :login => params[:user_attributes][:email], :email => params[:user_attributes][:email], :first_name => params[:user_attributes][:first_name], :last_name => params[:user_attributes][:last_name], :password => params[:user_attributes][:password], :password_confirmation => params[:user_attributes][:password_confirmation])
 
       redirect_to root_path
     else
