7836d9acbd284d28943988a3b90435a29e9e2e7b
Arco Overbeek <arcooverbeek@MB-Pro-van-Arco-Overbeek.local>
2011-05-14 13:48:42 +0200

    changed the new team for to include user fields

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,7 +6,11 @@ class TeamsController < ApplicationController
   
   def create
     team = Team.new(params[:team])
+    
+    
     if team.save
+      user = team.users.build(:login => login_name, :email => school.contact_email, :first_name => school.contact_first_name, :last_name => school.contact_last_name, :password => temp_password, :password_confirmation => temp_password)
+
       redirect_to root_path
     else
       render :new
